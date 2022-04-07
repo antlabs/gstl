77197bab456fc218cc26e7ce2f814c9d803eb017
guonaihong <guonaihong@qq.com>
2022-04-07 23:33:11 +0800

    更新代码

--- vecdeque/vecdeque.go
+++ vecdeque/vecdeque.go
@@ -1,6 +1,7 @@
 package vecdeque
 
 import (
+	"errors"
 	"fmt"
 	"github.com/guonaihong/gstl/cmp"
 	"math"
@@ -17,9 +18,13 @@ const (
 	MINIMUM_CAPACITY uint = 1 // 2 - 1
 )
 
+var (
+	ErrNoData = errors.New("no data")
+)
+
 type VecDeque[T any] struct {
-	//总是指向可以读取的第一个元素
-	//head只是指向应该写入数据的位置
+	// tail 总是指向可以读取的第一个元素
+	// head 只是指向应该写入数据的位置
 	// 如果tail == head, 则缓存区为空. 环形缓冲区的长度定义为两者之间的距离
 	tail uint
 	head uint
@@ -42,28 +47,57 @@ func (v *VecDeque[T]) IsFull() bool {
 	return v.Cap()-v.Len() == 1
 }
 
-//TODO
+// TODO
 func (v *VecDeque[T]) Len() int {
 	return 0
 }
 
 func (v *VecDeque[T]) grow() *VecDeque[T] {
-
 	return v
+}
+
+// 判断VecDeque
+func (v *VecDeque[T]) IsEmpty() bool {
+	return v.tail == v.head
+}
+
+//
+func (v *VecDeque[T]) PopBack() (value T, err error) {
+	if v.IsEmpty() {
+		err = ErrNoData
+		return
+	}
+
+	v.head = v.wrapSub(v.head, 1)
+	value = v.buf[v.head]
+	return
+}
+
+func (v *VecDeque[T]) PopFront() (value T, err error) {
+	if v.IsEmpty() {
+		err = ErrNoData
+		return
+	}
+
+	value = v.buf[v.tail]
+	v.tail = v.wrapAdd(v.tail, 1)
+	return
 }
 
 // 将一个元素添加到VecDeque 后面
 func (v *VecDeque[T]) PushBack(value T) {
+
+	// 先检查是否满了
 	if v.IsFull() {
+		// 满了就扩容
 		v.grow()
 	}
 	head := v.head
+
+	// 修改head的值
 	v.head = v.wrapAdd(v.head, uint(1))
 
 	v.buf[head] = value
-
-	// 先检查是否满了
-	// 没有满就扩容
 	// 修改head值
 }
 
@@ -77,6 +111,10 @@ func (v *VecDeque[T]) cap() int {
 
 func (v *VecDeque[T]) Cap() int {
 	return v.cap() - 1
+}
+
+func (v *VecDeque[T]) wrapSub(index uint, subtrahend uint) uint {
+	return v.wrapIndex(index - subtrahend)
 }
 
 func (v *VecDeque[T]) wrapAdd(index uint, addend uint) uint {
@@ -97,6 +135,7 @@ func wrapIndex(index uint, size uint) uint {
 }
 
 // TODO 优化下
+// 使用更好的算法计算
 func nextPowOfTwo(n uint) uint {
 
 	for i := 1; i < 32; i++ {
