f44ccf6600d16506083a73310844aa69abfa1c1e
guonaihong <guonaihong@qq.com>
2022-06-21 21:59:05 +0800

    新加和map一起的benchmark数据

--- skiplist/skiplist_bench_test.go
+++ skiplist/skiplist_bench_test.go
@@ -10,19 +10,10 @@ import (
 // goarch: amd64
 // pkg: github.com/guonaihong/gstl/skiplist
 // cpu: Intel(R) Core(TM) i7-1068NG7 CPU @ 2.30GHz
-// BenchmarkGet-8   	1000000000	         0.6353 ns/op
+// BenchmarkGet-8      	1000000000	         0.7746 ns/op
+// BenchmarkGetStd-8   	1000000000	         0.7847 ns/op
 // PASS
-// ok  	github.com/guonaihong/gstl/skiplist	71.838s
-
-// goos: darwin
-// goarch: amd64
-// pkg: github.com/guonaihong/gstl/skiplist
-// cpu: Intel(R) Core(TM) i7-1068NG7 CPU @ 2.30GHz
-// BenchmarkGet-8
-// 1000000000	         0.7430 ns/op	       0 B/op	       0 allocs/op
-// PASS
-// ok  	github.com/guonaihong/gstl/skiplist	114.326s
-
+// ok  	github.com/guonaihong/gstl/skiplist	178.377s
 // 五百万数据的Get操作时间
 func BenchmarkGet(b *testing.B) {
 	max := 1000000.0 * 5
@@ -35,6 +26,24 @@ func BenchmarkGet(b *testing.B) {
 
 	for i := 0.0; i < max; i++ {
 		v := set.Get(i)
+		if v != i {
+			panic(fmt.Sprintf("need:%f, got:%f", i, v))
+		}
+	}
+}
+
+func BenchmarkGetStd(b *testing.B) {
+
+	max := 1000000.0 * 5
+	set := make(map[float64]float64, int(max))
+	for i := 0.0; i < max; i++ {
+		set[i] = i
+	}
+
+	b.ResetTimer()
+
+	for i := 0.0; i < max; i++ {
+		v := set[i]
 		if v != i {
 			panic(fmt.Sprintf("need:%f, got:%f", i, v))
 		}
