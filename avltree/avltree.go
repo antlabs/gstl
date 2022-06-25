fceda0d03d3a3c572a24165e7b918fbb1b628b3b
guonaihong <guonaihong@qq.com>
2022-06-25 21:21:48 +0800

    avltree: Get

--- avltree/avltree.go
+++ avltree/avltree.go
@@ -1,6 +1,12 @@
 package avltree
 
-import "golang.org/x/exp/constraints"
+import (
+	"errors"
+
+	"golang.org/x/exp/constraints"
+)
+
+var ErrNotFound = errors.New("avltree: not found value")
 
 type Node[K constraints.Ordered, V any] struct {
 	left   *Node[K, V]
@@ -23,4 +29,31 @@ type AvlTree[K constraints.Ordered, V any] struct {
 
 func (a *AvlTree[K, V]) New() {
 
+}
+
+func (a *AvlTree[K, V]) First() (v V) {
+	return
+}
+
+func (a *AvlTree[K, V]) Last() (v V) {
+	return
+}
+
+// 从avl tree找到需要的值
+func (a *AvlTree[K, V]) Get(k K) (v V, err error) {
+	n := a.root
+	for n != nil {
+		if n.key == k {
+			return n.val, nil
+		}
+
+		if k > n.key {
+			n = n.right
+		} else {
+			n = n.left
+		}
+	}
+
+	err = ErrNotFound
+	return
 }
