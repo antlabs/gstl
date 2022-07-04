c91e2feea8b7c579df5d4afa41df631e36aa798b
guonaihong <guonaihong@qq.com>
2022-07-05 00:15:15 +0800

    rbtree: 更新下节点的定义

--- rbtree/btree.go
+++ rbtree/btree.go
@@ -2,6 +2,22 @@ package rbtree
 
 import "golang.org/x/exp/constraints"
 
+// 元素
+type pair[K constraints.Ordered, V any] struct {
+	val V
+	key K
+}
+
+type node[K constraints.Ordered, V any] struct {
+	left   *node[K, V]
+	right  *node[K, V]
+	parent *node[K, V]
+	pair[K, V]
+	// red/black 可以用一个变量实现, 为了代码简单清晰, 这里不考虑一个字节的收益, 放弃使用位操作
+	red   bool
+	black bool
+}
+
 // 红黑树
 type RBtree[K constraints.Ordered, V any] struct {
 }
