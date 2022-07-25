15da0637ac458e5ee69239957853c3a283aba8ba
guonaihong <guonaihong@qq.com>
2022-07-26 00:49:31 +0800

    更新

--- trie/trie_map.go
+++ trie/trie_map.go
@@ -1,25 +1,46 @@
 package trie
 
+// apache 2.0 guonaihong
+
 type Trie[V any] struct {
 	v        V
-	children map[rune]V
-}
-
-type node[V any] struct {
-
-  r rune
+	children map[rune]*Trie[V]
 }
 
 func New[V any]() *Trie[V] {
 	return &Trie[V]{}
 }
 
-func (t *Trie[V]) Set(k string) {
-
+func (t *Trie[V]) Set(k string, v V) {
+	_, _ = t.SetWithPrev(k, v)
 }
 
-func (t *Trie[V]) Get(k string) {
+func (t *Trie[V]) SetWithPrev(k string, v V) (prev V, replaced bool) {
+	n := t
+	for _, r := range k {
+		c := n.children[r]
+		if c == nil {
+			if n.children == nil {
+				n.children = map[rune]*Trie[V]{}
+			}
+			c = &Trie[V]{}
+		}
+		n = c
+	}
 
+	// TODO
+	return
+}
+
+func (t *Trie[V]) Get(k string) (v V) {
+	n := t
+	for _, r := range k {
+		n = n.children[r]
+		if n == nil {
+			return
+		}
+	}
+	return n.v
 }
 
 func (t *Trie[V]) Delete(k string) {
