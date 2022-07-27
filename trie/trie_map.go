488abfddf329d0139dd2f3ced649df8ff285a3f5
guonaihong <guonaihong@qq.com>
2022-07-28 00:30:46 +0800

    trie: delete

--- trie/trie_map.go
+++ trie/trie_map.go
@@ -1,5 +1,7 @@
 package trie
 
+import "unicode/utf8"
+
 // apache 2.0 guonaihong
 
 type Trie[V any] struct {
@@ -37,6 +39,19 @@ func (t *Trie[V]) SetWithPrev(k string, v V) (prev V, replaced bool) {
 	return
 }
 
+func (t *Trie[V]) HasPrefix(k string) bool {
+
+	n := t
+	for _, r := range k {
+		n := n.children[r]
+		if n == nil {
+			return false
+		}
+	}
+
+	return true
+}
+
 func (t *Trie[V]) Get(k string) (v V) {
 	n := t
 	for _, r := range k {
@@ -48,6 +63,46 @@ func (t *Trie[V]) Get(k string) (v V) {
 	return n.v
 }
 
-func (t *Trie[V]) Delete(k string) {
+func (t *Trie[V]) isLeaf() bool {
+	return len(t.children) == 0
+}
 
+// 记录删除的过程
+type recogNode[V any] struct {
+	r rune
+	n *Trie[V]
+}
+
+// 删除有两种方法, 这里先选择第1种，后面有时间再压测下第二种效率如何
+// 1.记录rune和节点，删除这个节点。如果是子节点，再回溯删除
+// 2.声明一个parent指针，不记录过程节点，直接p = n.parent; p != nil; p=p.parent 回溯删除
+func (t *Trie[V]) Delete(k string) {
+	recog := make([]recogNode[V], 0, utf8.RuneCountInString(k))
+
+	var v V
+	n := t
+
+	for _, r := range k {
+		recog = append(recog, recogNode[V]{r, n})
+		n = n.children[r]
+		if n == nil {
+			return
+		}
+	}
+
+	n.v = v
+	n.set = false
+
+	if !n.isLeaf() {
+		return
+	}
+
+	for last := len(recog) - 1; last >= 0; last-- {
+		p := recog[last].n
+		delete(p.children, recog[last].r)
+
+		if !p.isLeaf() {
+			return
+		}
+	}
 }
