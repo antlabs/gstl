ed71c2fe086f81ef2a7f55b3aa8fc2ee50a47b96
guonaihong <guonaihong@qq.com>
2022-08-28 15:09:51 +0800

    radix:get

--- radix/radix.go
+++ radix/radix.go
@@ -1,6 +1,9 @@
 package radix
 
 import (
+	"strings"
+	"unicode/utf8"
+
 	"github.com/guonaihong/gstl/api"
 	"github.com/guonaihong/gstl/vec"
 )
@@ -9,8 +12,9 @@ var _ api.Trie[int] = (*Radix[int])(nil)
 
 // 健值对
 type pair[V any] struct {
-	val V
-	key string
+	val   V
+	key   string
+	isSet bool
 }
 
 // 边
@@ -34,7 +38,7 @@ type Radix[V any] struct {
 
 // 获取
 func (r *Radix[V]) Get(k string) (v V) {
-
+	v, _ = r.GetWithBool(k)
 	return
 }
 
@@ -57,9 +61,51 @@ func (r *Radix[V]) HasPrefix(k string) (ok bool) {
 	return
 }
 
+func (n *node[V]) children(r rune) (v *node[V], found bool) {
+	index, found := n.find(r)
+	if !found {
+		return
+	}
+	return n.edges.Get(index).node, true
+}
+
+func (n *node[V]) find(r rune) (index int, found bool) {
+
+	index = n.edges.SearchFunc(func(elem edge[V]) bool { return r < elem.label })
+	if index > 0 && n.edges.Get(index-1).label >= r {
+		return index - 1, true
+	}
+
+	return index, false
+}
+
 // 获取返回bool
 func (r *Radix[V]) GetWithBool(k string) (v V, found bool) {
-	return
+	n := r.root
+
+	for {
+
+		// k 消费完，要不找到，要不找不到
+		if len(k) == 0 {
+			if n.isSet {
+				return n.val, true
+			}
+			return
+		}
+
+		rune, _ := utf8.DecodeLastRuneInString(k)
+		n, found = n.children(rune)
+		if !found {
+			return
+		}
+
+		if strings.HasPrefix(k, n.prefix) {
+			k = k[len(n.prefix):]
+			continue
+		}
+		return
+	}
+
 }
 
 // 删除
